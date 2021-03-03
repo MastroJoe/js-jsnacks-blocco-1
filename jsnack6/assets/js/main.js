1b5b7fa966fefb941ae4ffc7ffeb5699a19bd27d
Giorgio <giorgio.pignattari@gmail.it>
2021-03-03 16:42:03 +0100

    finito js6

--- jsnack6/assets/js/main.js
+++ jsnack6/assets/js/main.js
@@ -1,1 +1,8 @@
+// chiedo all'utente di inserire un numero
+var n = parseInt(prompt("Inserisci un numero: "));
 
+// stampo il cubo dei primi n numeri
+for (var i = 1; i <= n ; i++) {
+  var risultato = Math.pow(i, 3);
+  console.log(risultato);
+}
