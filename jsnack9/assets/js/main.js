54cfb9b42500fd600617660c314c9708aba4d8ec
Giorgio <giorgio.pignattari@gmail.it>
2021-03-03 17:47:00 +0100

    finito js9

---
+++ jsnack9/assets/js/main.js
@@ -0,0 +1,14 @@
+// calcolo la somma dei primi 10 numeri
+var somma = 0;
+
+for (var i = 1; i <= 10; i++) {
+  var somma = somma + i;
+  console.log(i);
+}
+
+console.log(somma);
+
+// calcolo la media dei primi 10 numeri
+var media = somma/10;
+
+console.log(media);
